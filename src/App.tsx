401cd0382263ea9bba2aa0103bcad25a1bdf5cc0
Dhany Laudza <67975936+danilaudza@users.noreply.github.com>
2023-03-05 14:19:34 +0700

    test form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import copy from "copy-to-clipboard";  
 import './App.css';
 
 function App() {
+  const [name,setName] = useState("")
+  const [text,setText] = useState("")
+  const [copyText,setCopy] = useState("")
+
+  useEffect(() => {
+    setCopy(`Name : ${name}\nText : ${text}`)
+    console.log(copyText)
+  },[name,text]);
+
+  const copyToClipboard = () => {
+
+    copy(copyText);
+    alert(`You have copied "${copyText}"`);
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col'>
+      <p>Name</p>
+      <input onChange={e => setName(e.target.value)} className="border-2"/>
+      <p>Text</p>
+      <input onChange={e => setText(e.target.value)} className="border-2"/>
+      <div className='bg-gray-600'>
+          <p>Name : {name}</p>
+          <p>Text : {text}</p>
+      </div>
+
+      <button className='bg-red-600' onClick={copyToClipboard}>
+               Copy to Clipboard
+      </button>
     </div>
   );
 }
